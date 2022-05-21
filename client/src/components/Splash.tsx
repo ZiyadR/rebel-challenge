660331c04541787d9a5a14c0d0000d46eac72131
Ziyad Rabeh <zrabeh@spreevel.com>
2022-05-21 13:28:29 -0400

    update splash screen

--- client/src/components/Splash.tsx
+++ client/src/components/Splash.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { CircularProgress, VStack, Image } from '@chakra-ui/react';
+import { CircularProgress, VStack } from '@chakra-ui/react';
 
 const Splash = () => <VStack spacing={8} justifyContent="center" height="100vh">
-    <img alt="logo" src="/logo.png" width="100px" />
     <CircularProgress isIndeterminate thickness="5px" size="150px" color='teal' />
 </VStack>;
 
