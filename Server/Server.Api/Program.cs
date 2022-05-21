fe4ecdbd6a68e36619e2f3c9a57256390a1c3b6f
Ziyad Rabeh <zrabeh@spreevel.com>
2022-05-21 11:34:56 -0400

    add api project

---
+++ Server/Server.Api/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options => options.AddPolicy("AllowAnyOrigin",
+    b => b
+        .SetIsOriginAllowed(_ => true)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()));
+
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+if (connectionString != null)
+    builder.Services.AddDbContext<StreamingContext>(options => options.UseSqlServer(connectionString));
+
+
+// Configure the HTTP request pipeline
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAnyOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
